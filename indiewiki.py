2461f4f39d53911794d907894941afdfa61b3280
Patrick <pcr@doggo.dance>
2026-05-12 17:08:51 -0500

    fix invalid rewrites

--- indiewiki.py
+++ indiewiki.py
@@ -7,8 +7,9 @@ def main():
     redirect_list = pandas.read_json(
         "https://raw.githubusercontent.com/KevinPayravi/indie-wiki-buddy/refs"
         "/heads/main/data/sitesEN.json").convert_dtypes()
-    # Build rules using a list comprehension instead of nested loops
-    rules = [f"{origin['origin_base_url']}^$dnsrewrite={row['destination_base_url']}\n" for _, row
+    rules = [
+        f"{origin['origin_base_url']}^$dnsrewrite={row['destination_base_url'].partition('/')[0]}\n"
+        for _, row
         in redirect_list.iterrows() for origin in row["origins"]]
     output_dir = pathlib.Path("output")
     if not output_dir.exists():
